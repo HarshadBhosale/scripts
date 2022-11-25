4bbb0262a80fc6386fe9954a079ae98908cf3178
HarshadBhosale <harshad.bhosale@cogoport.com>
2022-11-25 12:07:50 +0530

    added yarn install feature to function

--- cogo_repo_setup.sh
+++ cogo_repo_setup.sh
@@ -2,6 +2,14 @@
 
 CHECK_MARK="\033[1;32m\xE2\x9C\x94\033[0m"
 repo=(cogo-ml front-saas cogo-product cogoport-back)
+repo_need_yarn=(front-saas cogo-product)
+END_MESSAGE='Made with ❤️  From Harshosale'
+
+function checkForYarn {
+    if [[ "${repo_need_yarn[*]}" =~ "$1" ]]; then
+        yarn
+    fi
+}
 
 cd ~/Desktop &&
 
@@ -12,35 +20,36 @@ do
     if [ ! -d "$FILE" ]; then
         echo -ne "\033[1;31m\xE2\x9D\x8C\033[0m $FILE does not exist on Desktop. Press y to clone it? "
         read clone_response
-        echo ''
+        echo
 
         if [ "$clone_response" == "y" ] ; then
             echo "Cloning $FILE...."
             git clone git@github.com:Cogoport/$FILE.git
             echo -e "\r${CHECK_MARK} Cloned $FILE"
+
             cd $FILE &&
-            if [ "$FILE" == "front-saas" ] || [ "$FILE" == "cogo-product" ] ; then
-                yarn install
-            fi
+            checkForYarn $FILE
         fi
     else
-        echo "" &&
+        echo &&
         echo "#####-----$FILE-----#####" &&
-        echo "" &&
+        echo &&
 
         cd $FILE &&
         git pull &&
-        if [ "$FILE" == "front-saas" ] || [ "$FILE" == "cogo-product" ] ; then
-            yarn install
-        fi
+
+        checkForYarn $FILE
+
         cd ..
 
-        echo "" &&
+        echo &&
         echo -e "${CHECK_MARK} $FILE updated " &&
-        echo "" &&
+        echo &&
         sleep 0.1
     fi
-
 done
 
-echo 'Made with ❤️  From Harshosale'
+echo $END_MESSAGE
+
+
+
